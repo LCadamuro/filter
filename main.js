cedeabff7d55342390a36024ffdecc0872c79bd5
LCadamuro <126515552+LCadamuro@users.noreply.github.com>
2023-12-19 10:29:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preLoad() {
+
+}
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(11, 250);
+
+    video = createCapture(VIDEO);
+    video.hide;
+
+    tint_color = "";
+}
+function draw() {
+    image(video, 0, 0, 640, 480);
+    tint(tint_color);
+}
+function take_snapshot() {
+    save('filtro_lucas.png');
+}
+function filterTint() {
+    tint_color = document.getElementById("color_name").value;
+}
